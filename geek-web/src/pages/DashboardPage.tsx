4ba852e7ee09db96f0d338161bb0fdc931e5a004
alexander <nesterchuksasha95@gmail.com>
2021-03-23 09:55:20 +0200

    dashboard item

--- geek-web/src/pages/DashboardPage.tsx
+++ geek-web/src/pages/DashboardPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { DashboardItem } from '../components/dashboard/DashboardItem'
 export const DashboardPage = () => {
   return (
     <div>
       <h2>DashboardPage</h2>
+      <div className="row">
+        <div className="col m3">
+          <DashboardItem />
+        </div>
+      </div>
     </div>
   )
 }
